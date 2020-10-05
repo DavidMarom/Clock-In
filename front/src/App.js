1b02851b419f7346866ac6a4a7c6abef93a9d83a
David Marom <davidmarom@hotmail.com>
2020-10-06 00:04:05 +0300

    back and front init

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi Joe</h1>
+    </div>
+  );
+}
+
+export default App;
