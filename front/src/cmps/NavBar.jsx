eee85ff411437210c1e5ffac92da6b0439a86ff2
David Marom <davidmarom@hotmail.com>
2020-10-11 17:12:20 +0300

    css work

--- front/src/cmps/NavBar.jsx
+++ front/src/cmps/NavBar.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 
+const logo = require('../assets/img/joe-logo.jpg')
+
 export function NavBar() {
     return (
         <div>
-            <NavLink to="/">Home</NavLink> -
-            <NavLink to="/page-01">Page 01</NavLink> -
-            <NavLink to="/page-02">Page 02</NavLink>
+            <div className="outter-container nav-bar drop-shadow">
+                <div className="inner-container">
 
+                    <div className="ra">
+                        <div className="rb">
+                            <NavLink to="/"><img src={logo} /></NavLink>
+                        </div>
+
+                        <div className="ca">
+                            <div className="ra">
+                                <NavLink to="/page-01">Page-01</NavLink>
+                                <NavLink to="/page-02">Page-02</NavLink>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+            </div>
         </div>
     )
 }
--- front/src/index.js
+++ front/src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 import { HashRouter as Router } from 'react-router-dom';
 
+import "./assets/css/main.css";
 
 ReactDOM.render(
   <React.StrictMode>
