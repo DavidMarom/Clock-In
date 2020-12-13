8ec04c03bfa4b49c51d56a4120b9d1d5cedd67ef
DavidMarom <davidmarom.pro@gmail.com>
2020-12-13 08:28:01 +0200

    clock in-out is using DB data now

--- front/src/pages/Punch.jsx
+++ front/src/pages/Punch.jsx
@@ -1,22 +1,17 @@
 import React from 'react'
 import moment from 'moment'
 
-export const Punch = () => {
+import { connect } from 'react-redux';
+
+const _Punch = (props) => {
 
 
-    // dummy data:
-    var hours = {
-        20: {
-            12: {
-                1: [1606806000, 1606809600],
-                2: [1606806000, 1606806000]
-            }
-        }
-    }
+    const { loggedInUser } = props;
+    const hours = loggedInUser.hours;
 
     const currYear = 20;
     const currMonth = 12;
-
+    
     return (
         <div>
             <h1>Clock in/out</h1>
@@ -34,6 +29,15 @@ export const Punch = () => {
                 <p className="tc">{moment.unix(hours[currYear][currMonth][1][1]).format('hh:mm')}</p>
                 <p className="tc">{moment.unix(hours[currYear][currMonth][1][1] - hours[currYear][currMonth][1][0] - 50400).format('hh:mm')}</p>
             </div>
+            
+            <div className="table2">
+                <p className="tc">02</p>
+                <p className="tc">{moment.unix(hours[currYear][currMonth][2][0]).format('hh:mm')}</p>
+                <p className="tc">{moment.unix(hours[currYear][currMonth][2][1]).format('hh:mm')}</p>
+                <p className="tc">{moment.unix(      (hours[currYear][currMonth][2][1] - hours[currYear][currMonth][2][0])     -    50400).format('hh:mm')}</p>
+            </div>
+
+            
             {/* {for (var key in hours[currYear][currMonth]) {
                 if (hours.[currYear].[currMonth].hasOwnProperty(key)) {
                 console.log(key + " -> " + hours[currYear][currMonth][key]);
@@ -45,9 +49,23 @@ export const Punch = () => {
                 <p className="tc"></p>
                 <p className="tc"></p>
                 <p className="tc"></p>
-                <p className="tc">17:24</p>
+                <p className="tc">{moment.unix((hours[currYear][currMonth][2][1] - hours[currYear][currMonth][2][0]) + (hours[currYear][currMonth][1][1] - hours[currYear][currMonth][1][0]) - 50400).format('hh:mm') }</p>
 
             </div>
         </div>
     )
 }
+
+const mapStateToProps = state => {
+    return {
+        users: state.user.users,
+        loggedInUser: state.user.loggedInUser,
+        isLoading: state.system.isLoading
+    };
+};
+
+
+export const Punch = connect(mapStateToProps, null)(_Punch);
+
+
+
