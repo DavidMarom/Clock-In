18932a447cb7fc99105ad8df5f45e31b886a0eef
DavidMarom <davidmarom.pro@gmail.com>
2020-12-15 16:01:28 +0200

    clock in-out is iterating over the month days and renders table dynamically using map function

--- front/src/pages/Punch.jsx
+++ front/src/pages/Punch.jsx
@@ -1,81 +1,45 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import moment from 'moment'
 import { loadUsers, login, logout, signup, updateUser, getUserById } from '../store/actions/userActions';
 import { connect } from 'react-redux';
 
 const _Punch = (props) => {
-    // useEffect(() => {
-    //     console.log('aaaaaaaaaaaaaaaa');
-    // }, loggedInUser);
-
-    var { loggedInUser } = props;
-    const hours = loggedInUser.hours;
-
     const currYear = 20;
     const currMonth = 12;
-
-    // if (!loggedInUser.hours) {
-    //     loggedInUser = {
-    //         ...loggedInUser,
-    //         hours: {
-    //             20: {
-    //                 12: {
-    //                     1: [1606806000, 1606809600],
-    //                     2: [1606806000, 1606813000]
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     props.updateUser(loggedInUser);
-    // }
+    let sum = 0;
+    var { loggedInUser } = props;
+    const hours = Object.entries(loggedInUser.hours[currYear][currMonth]);
 
     return (
         <div>
             <h1>Clock in/out</h1>
-            {
-                (loggedInUser.hours
-                    ?
-                    <div>
-
-                        <div className="table-head2">
-                            <p className="tch">Day</p>
-                            <p className="tch">In</p>
-                            <p className="tch">Out</p>
-                            <p className="tch">Total</p>
+            <div>
+                <div className="table-head2">
+                    <p className="tch">Day</p>
+                    <p className="tch">In</p>
+                    <p className="tch">Out</p>
+                    <p className="tch">Total</p>
+                </div>
+                {hours.map((day, idx) => {
+                    sum += (day[1][1] - day[1][0]);
+                    return (
+                        <div key={idx} className="table2">
+                            <p className="tc">{day[0][0]}</p>
+                            <p className="tc">{moment.unix(day[1][0]).format('hh:mm')}</p>
+                            <p className="tc">{moment.unix(day[1][1]).format('hh:mm')}</p>
+                            <p className="tc">{moment.unix((day[1][1] - day[1][0]) - 50400).format('hh:mm')}  </p>
                         </div>
+                    )
+                })}
 
-                        <div className="table2">
-                            <p className="tc">01</p>
-                            <p className="tc">{moment.unix(hours[currYear][currMonth][1][0]).format('hh:mm')}</p>
-                            <p className="tc">{moment.unix(hours[currYear][currMonth][1][1]).format('hh:mm')}</p>
-                            <p className="tc">{moment.unix(hours[currYear][currMonth][1][1] - hours[currYear][currMonth][1][0] - 50400).format('hh:mm')}</p>
-                        </div>
+                <div className="table-head2">
+                    <p className="tch"></p>
+                    <p className="tch"></p>
+                    <p className="tch"></p>
+                    <p className="tch">{moment.unix(sum - 50400).format('hh:mm')}</p>
+                </div>
 
-                        <div className="table2">
-                            <p className="tc">02</p>
-                            <p className="tc">{moment.unix(hours[currYear][currMonth][2][0]).format('hh:mm')}</p>
-                            <p className="tc">{moment.unix(hours[currYear][currMonth][2][1]).format('hh:mm')}</p>
-                            <p className="tc">{moment.unix((hours[currYear][currMonth][2][1] - hours[currYear][currMonth][2][0]) - 50400).format('hh:mm')}</p>
-                        </div>
-
-                        <div className="table-head2">
-                            <p className="tc"></p>
-                            <p className="tc"></p>
-                            <p className="tc"></p>
-                            <p className="tc">{moment.unix((hours[currYear][currMonth][2][1] - hours[currYear][currMonth][2][0]) + (hours[currYear][currMonth][1][1] - hours[currYear][currMonth][1][0]) - 50400).format('hh:mm')}</p>
-
-                        </div>
-                    </div>
-
-                    :
-
-                    <h2>You dont have hours data yet. This feature is in development</h2>
-                )
-
-
-            }
-
-
+            </div>
         </div >
     )
 }
@@ -88,12 +52,8 @@ const mapStateToProps = state => {
     };
 };
 
-
 const mapDispatchToProps = {
     login, logout, signup, loadUsers, updateUser, getUserById
 };
 
 export const Punch = connect(mapStateToProps, mapDispatchToProps)(_Punch);
-
-
-
