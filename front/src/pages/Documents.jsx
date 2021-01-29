206660477e67c0913aaa6b33c826b68ea15b44c4
karim arkia <karimarkia@gmailcom>
2021-01-29 20:25:52 +0200

    working on documents component

--- front/src/pages/Documents.jsx
+++ front/src/pages/Documents.jsx
@@ -1,24 +1,41 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setPageName } from "../store/actions/userActions";
-const docs_img = require("../assets/img/docs.jpg");
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setPageName } from '../store/actions/userActions'
+const docs_img = require('../assets/img/docs.jpg')
 
 export const Documents = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
     // Hook option 2: dispatch({type:'PAGE_NAME',name:'Documents'})
-    dispatch(setPageName("Documents"));
-  });
+    dispatch(setPageName('Documents'))
+  })
 
   return (
     <div>
       <h1>Documents: </h1>
-      <img src={docs_img} alt=""></img>
+      {/* <img src={docs_img} alt=""></img> */}
       <div className="table-wrapper">
-       test test
-        
+        <h2 className="Timesheet">Documents</h2>
+        <button className="upload-btn">
+          <i className="fas fa-upload"></i> Upload
+        </button>
+        <button className="folder-btn">
+          <i className="fas fa-folder-plus"></i>
+        </button>
+
+        <div className="tickets">
+          <p className="ticket-name">Signed Documents (2)</p>
+        </div>
+        <hr className="hr" />
+        <div className="tickets">
+          <p className="ticket-name">Resumes And Applications (0)</p>
+        </div>
+        <hr className="hr" />
+        <div className="tickets">
+          <p className="ticket-name">Tasklist Attachments (1)</p>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
